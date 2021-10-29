e672887080e0134d9341ff589cd0e46a20bb0a53
YeJinii <yeagin09@gmail.com>
2021-10-30 01:34:18 +0900

    #5 Grid Card Component

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
 import MainImage from './Sections/MainImage';
-
+import axios from 'axios';
+import GridCards from '../commons/GridCards';
+import {Row} from 'antd';
 function LandingPage() {
 
     const [Movies,setMovies] = useState([])
@@ -15,7 +17,7 @@ function LandingPage() {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
-            setMovies([response.results])
+            setMovies([...response.results])
             setMainMovieImage(response.results[0])
         })
 
@@ -38,7 +40,24 @@ function LandingPage() {
                 <h2>Movies by latest</h2>
                 <hr />
 
-                {/*Movie Grid Cards */}
+                {/*Movie Grid Cards */} 
+                <Row gutter={[16,16]}>
+
+                    {Movies && Movies.map((movie, index)=> (
+                        <React.Fragment key={index}>
+                            <GridCards 
+                                image = {movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId = {movie.id} 
+                                movieName={movie.original_title}
+                            />
+
+                        </React.Fragment>
+                    ))}
+
+
+            
+            </Row>
 
             </div>
 
