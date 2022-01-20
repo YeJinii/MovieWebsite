96d812d6038d22315a039184c1aa6d92b70cd3df
YeJinii <yeagin09@gmail.com>
2022-01-21 01:59:10 +0900

    #7 Movie Detail 페이지 만들기

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,4 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'c29c8ab36b2848252488e98c47be9bc6';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
